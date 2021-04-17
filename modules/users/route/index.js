8bce5ff2e479cddf7f09af92c92dbc711cae2a20
bitanco <sayed.khaidirali@gmail.com>
2021-04-17 15:54:44 +0700

    update endpoint user

--- modules/users/route/index.js
+++ modules/users/route/index.js
@@ -6,4 +6,11 @@ router.route('/')
   .post(userController.create)
   .get(userController.getAllUser)
 
+/**
+ *ini kalo mau nambah2 url, dan ini base endpoint nya /user
+ *
+ */
+router.route('/verify')
+  .post()
+
 module.exports.UserRouter = router;
